56bbe354ff6c4bae8223362bb1226fb38509b419
Kimani Kamau <kimkamaug@gmail.com>
2024-09-11 20:44:59 +0300

    Made the monthly_savings variable into an integer

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
 monthly_income = int(input('Enter your monthly income: '))
 monthly_expenses = int(input('Enter your monthly expenses: '))
-monthly_savings = (monthly_income - monthly_expenses)
+monthly_savings = int(float(monthly_income) - float(monthly_expenses))
 annual_interest_rate = 0.05
 projected_savings = int(monthly_savings * 12 + (monthly_savings * 12 * annual_interest_rate))
 print(f'Your monthly savings are ${monthly_savings}')
