4e44ae859e1a853a6700f979657c9fb7ec9b0781
Kimani Kamau <kimkamaug@gmail.com>
2024-09-11 20:22:52 +0300

    Calculated and provided feedback on a user's monthly savings without applying conditional statements

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,6 @@
+monthly_income = int(input('Enter your monthly income: '))
+monthly_expenses = int(input('Enter your monthly expenses: '))
+monthy_savings = monthly_income - monthly_expenses
+annual_interest_rate = 0.05
+projected_savings = int(monthy_savings * 12 + (monthy_savings * 12 * annual_interest_rate))
+print(f'Projected savings after one year, with interest, is: ${projected_savings}')
