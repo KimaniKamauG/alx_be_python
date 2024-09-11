2c283909a37b94dc191d73a095abeb30ebb7294b
Kimani Kamau <kimkamaug@gmail.com>
2024-09-11 21:01:03 +0300

    Defined the annual rate before the monthly savings

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 monthly_income = float(input('Enter your monthly income: '))
 monthly_expenses = float(input('Enter your monthly expenses: '))
-monthly_savings = int(float(monthly_income) - float(monthly_expenses))
 annual_interest_rate = 0.05
+monthly_savings = int(float(monthly_income) - float(monthly_expenses))
 projected_savings = int(monthly_savings * 12 + (monthly_savings * 12 * annual_interest_rate))
 print(f'Your monthly savings are ${monthly_savings}.')
 print(f'Projected savings after one year, with interest, is: ${projected_savings}.')
