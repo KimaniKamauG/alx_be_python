9d837343238aab3b6c96b88fc998813dc8b32c7c
Kimani Kamau <kimkamaug@gmail.com>
2024-09-12 20:38:56 +0300

    Made the final print variables into strings

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -3,5 +3,5 @@ monthly_expenses = int(input('Enter your monthly expenses: '))
 annual_interest_rate = 0.05
 monthly_savings = (monthly_income - monthly_expenses or float(monthly_income) - float(monthly_expenses))
 projected_savings = int(monthly_savings * 12 + (monthly_savings * 12 * annual_interest_rate))
-print(f'Your monthly savings are ${monthly_savings}.')
-print(f'Projected savings after one year, with interest, is: ${projected_savings}.')
+print(f'Your monthly savings are ${str(monthly_savings)}.')
+print(f'Projected savings after one year, with interest, is: ${str(projected_savings)}.')
