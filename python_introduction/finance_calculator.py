758b57cc1df70525718cca65f7c9fd5c85490032
Kimani Kamau <kimkamaug@gmail.com>
2024-09-12 20:51:12 +0300

    Removed the interest variable

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,6 @@
 monthly_income = int(input('Enter your monthly income: '))
 monthly_expenses = int(input('Enter your monthly expenses: '))
-annual_interest_rate = 0.05
-monthly_savings = (monthly_income - monthly_expenses or float(monthly_income) - float(monthly_expenses))
-projected_savings = int(monthly_savings * 12 + (monthly_savings * 12 * annual_interest_rate))
+monthly_savings = (monthly_income - monthly_expenses)
+projected_savings = int(monthly_savings * 12 + (monthly_savings * 12 * 0.05))
 print(f'Your monthly savings are ${str(monthly_savings)}.')
 print(f'Projected savings after one year, with interest, is: ${str(projected_savings)}.')
