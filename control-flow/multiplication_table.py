285609d40d9f79246e147bdc1921e609bd95e5d7
Kimani Kamau <kimkamaug@gmail.com>
2024-09-17 22:03:25 +0300

    Removed the int() from the input function.

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,5 @@
-number = int(input("Enter a number to see its multiplications table: "))
+number = input("Enter a number to see its multiplications table: ")
+number = int(number)
 
 for i in range(1, 11):
     print(f'{number} * {i} = {number * i}')
