406efdaae7822a3799cee7fc620540a2e0ea1d4c
Kimani Kamau <kimkamaug@gmail.com>
2024-09-26 01:02:20 +0300

    Python script for arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+def perform_operation(num1, num2, operation):
+    match operation:
+        case 'add':
+            result = num1 + num2
+        case 'subtract':
+            result = num1 - num2
+        case 'multiply':
+            result = num1 * num2
+        case 'divide':
+            if num1 == 0 or num2 == 0:
+                result = print('Cannot divide by zero')
+            else:
+                result = num1 / num2
+        case _:
+            print('Invalid Operation')
+    
+    return result
